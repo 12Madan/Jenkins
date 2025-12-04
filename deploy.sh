ace1df945ee79564d8b4bdca686ff017bccbcc94
Madan Reddy <116048916+12Madan@users.noreply.github.com>
2025-12-04 16:11:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+
 pipeline {
     agent any
 
@@ -6,31 +7,9 @@ pipeline {
     }
 
     stages {
-        stage('Clone Code') {
-            steps {
-                git branch: 'main', url: 'https://github.com/your-username/student-service.git'
-            }
-        }
-
-        stage('Print Environment') {
+        stage('Print Selected Env') {
             steps {
                 echo "Environment selected: ${params.ENV}"
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                script {
-                    if (params.ENV == 'dev') {
-                        echo "Deploying to DEV environment"
-                    }
-                    if (params.ENV == 'qa') {
-                        echo "Deploying to QA environment"
-                    }
-                    if (params.ENV == 'prod') {
-                        echo "Deploying to PROD environment"
-                    }
-                }
             }
         }
     }
